e555efb33e563f5d051f357adf168aaa6103939c
Alexander Rasyuk <th3arsk@yandex.ru>
2022-12-20 02:37:12 +0300

    added likes & add-button

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,20 @@
 const form = document.querySelector('.popup');
 const editButton = document.querySelector('.edit-button');
-const closeButton = document.querySelector('.close-button');
+const closeButton = document.querySelectorAll('.close-button');
 let nameInput = document.querySelector('#name');
 let jobInput = document.querySelector('#job');
 let profileNameElement = document.querySelector('.profile__name');
 let profileJobElement = document.querySelector('.profile__specialisation');
+const editFormContainer = document.querySelector('#edit-popup');
 
-function openForm() {
+function openEditForm() {
   form.classList.add('popup_opened');
+  editFormContainer.classList.remove('popup__container_closed');
   nameInput.value = profileNameElement.textContent;
   jobInput.value = profileJobElement.textContent;
 }
 
-editButton.addEventListener('click', openForm);
+editButton.addEventListener('click', openEditForm);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
@@ -23,50 +25,62 @@ function formSubmitHandler (evt) {
     closeForm();
 }
 
-const popupForm = document.querySelector('#popup');
+const popupForm = document.querySelector('#edit-popup');
 popupForm.addEventListener('submit', formSubmitHandler);
 
 function closeForm() {
   form.classList.remove('popup_opened');
+  let popupForm = document.querySelectorAll('.popup__container');
+  popupForm.forEach(element => element.classList.add('popup__container_closed'));
 }
 
-closeButton.addEventListener('click', closeForm);
+closeButton.forEach(element => element.addEventListener('click', closeForm));
 
-//let likeButton1 = document.querySelector('#like_1');
-//let likeButton2 = document.querySelector('#like_2');
-//let likeButton3 = document.querySelector('#like_3');
-//let likeButton4 = document.querySelector('#like_4');
-//let likeButton5 = document.querySelector('#like_5');
-//let likeButton6 = document.querySelector('#like_6');
+const addButton = document.querySelector('.add-button');
+const AddFormContainer = document.querySelector('#add-popup');
 
-//function like1() {
-//  likeButton1.classList.toggle('like-button_active');
-//}
-//likeButton1.addEventListener('click', like1);
+function openAddForm() {
+  form.classList.add('popup_opened');
+  AddFormContainer.classList.remove('popup__container_closed');
+}
 
-//function like2() {
-//  likeButton2.classList.toggle('like-button_active');
-//}
-//likeButton2.addEventListener('click', like2);
+addButton.addEventListener('click', openAddForm);
 
-//function like3() {
-//  likeButton3.classList.toggle('like-button_active');
-//}
-//likeButton3.addEventListener('click', like3);
+let likes = document.querySelectorAll('.like-button');
+likes.forEach(like => like.addEventListener('click', function () {
+  like.classList.toggle('like-button_active');
+}));
 
-//function like4() {
-//  likeButton4.classList.toggle('like-button_active');
-//}
-//likeButton4.addEventListener('click', like4);
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-//function like5() {
-//  likeButton5.classList.toggle('like-button_active');
-//}
-//likeButton5.addEventListener('click', like5);
 
-//function like6() {
-//  likeButton6.classList.toggle('like-button_active');
-//}
-//likeButton6.addEventListener('click', like6);
+
+
+
 
 
