c490c879cde7b6a089cf1de7bb4a6dccaea86437
Alexander Rasyuk <th3arsk@yandex.ru>
2023-01-14 18:33:41 +0300

    feat with innerHTNL

--- scripts/script.js
+++ scripts/script.js
@@ -18,10 +18,8 @@ editButton.addEventListener('click', openEditForm);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let nameValue = nameInput.value;
-    profileNameElement.textContent = nameValue;  
-    let jobValue = jobInput.value;
-    profileJobElement.textContent = jobValue;  
+    profileNameElement.textContent = nameInput.value;
+    profileJobElement.textContent = jobInput.value;  
     closeForm();
 }
 
@@ -45,11 +43,6 @@ function openAddForm() {
 }
 
 addButton.addEventListener('click', openAddForm);
-
-let likes = document.querySelectorAll('.like-button');
-likes.forEach(like => like.addEventListener('click', function () {
-  like.classList.toggle('like-button_active');
-}));
 
 const initialCards = [
   {
@@ -78,9 +71,86 @@ const initialCards = [
   }
 ];
 
+function like () {
+  let likes = document.querySelectorAll('.like-button');
+  likes.forEach(like => like.addEventListener('click', function () {
+    like.classList.toggle('like-button_active');
+  }));
+}
+
+function createCard (image, name) {
+  const card = `
+    <div class="element">
+      <img class="element__picture" src="${image}" alt="Картинка">
+      <h2 class="element__name">${name}</h2>
+      <button class="like-button" type="button"></button>
+      <button class="delete-button" type="button"></button>
+    </div>
+    `
+  return card;
+}
+
+//const deleteButton = element.querySelector('.delete-button');
+//const deleteCard = () => {
+//  element.remove();
+ //};
+ //deleteButton.addEventListener('click', deleteCard());
+
+let cardsElements = document.querySelector('.elements');
+function createCardImage () {
+  initialCards.forEach(item => {
+    const cardContent = createCard(item.link, item.name);
+    cardsElements.innerHTML += cardContent;
+  });
+
+  like ();
+}
+createCardImage ();
 
 
+let elements = document.querySelector('.elements');
+let cards = document.querySelector('.element');
+
+let cardName = document.querySelector('#card-name');
+let imageLink = document.querySelector('#image-link');
+
+function createNewCard (evt) {
+  evt.preventDefault();
+  cardsElements.insertAdjacentHTML(`afterbegin`,`
+  <div class="element">
+    <img class="element__picture" src="${imageLink.value}" alt="Картинка">
+    <h2 class="element__name">${cardName.value}</h2>
+    <button class="like-button" type="button"></button>
+    <button class="delete-button" type="button"></button>
+  </div>`) ;
+    like();
+    closeForm();
+}
+
+const createButton = document.querySelector('#add-popup');
+createButton.addEventListener('submit', createNewCard);
+
+let picture = document.querySelectorAll('.element__picture');
+
+function createImage () {
+  const image = `
+  <section class="picture-popup">
+    <div class="picture-popup__container">
+      <img class="picture-popup__image" src="https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg" alt="Картинка">
+      <p class="picture-popup__caption">Подпись Картинка</p>
+      <button class="close-button" type="button"></button>
+    </div>
+  </section>`
+
+  return image;
+}
+
+function imagePopup () {
+  const image = createImage();
+  document.innerHTML = image;
+}
+imagePopup ();
 
 
-
+  
 
